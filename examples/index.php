78257296534698ba425598eaaa006a84b258ce70
Marco Pivetta <ocramius@gmail.com>
2015-10-23 04:08:53 -0700

    More documentation for the examples

--- examples/index.php
+++ examples/index.php
@@ -10,12 +10,15 @@ use Psr\Http\Message\ResponseInterface;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // To run this example, you will need to run (in the project directory)
-// composer require zendframework/zend-expressive
-// composer require zendframework/zend-servicemanager
+// `composer require zendframework/zend-expressive`
+// `composer require zendframework/zend-servicemanager`
 
 // the example uses a symmetric key, but asymmetric keys can also be used.
 // $privateKey = new Key('file://private_key.pem');
 // $publicKey = new Key('file://public_key.pem');
+
+// simply run `php -S localhost:8888 index.php`
+// then point your browser at `http://localhost:8888/get`
 $privateKey = $publicKey = 'I do not care';
 
 $app = AppFactory::create();
