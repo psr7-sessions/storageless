b09c2b58ca0638c0ee61653e7b27e7381b8e6962
Marco Pivetta <ocramius@gmail.com>
2015-10-23 04:06:04 -0700

    Simplifying example

--- examples/index.php
+++ examples/index.php
@@ -9,30 +9,33 @@ use Psr\Http\Message\ResponseInterface;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+// To run this example, you will need to run (in the project directory)
+// composer require zendframework/zend-expressive
+// composer require zendframework/zend-servicemanager
+
+// the example uses a symmetric key, but asymmetric keys can also be used.
 // $privateKey = new Key('file://private_key.pem');
 // $publicKey = new Key('file://public_key.pem');
-// the example uses a symmetric key, but asymmetric keys can also be used.
 $privateKey = $publicKey = 'I do not care';
 
-$app->pipe(new SessionMiddleware(
-    new Sha256(),
-    $privateKey,
-    $publicKey,
-    \Dflydev\FigCookies\SetCookie::create('foo'),
-    new Parser(),
-    14400
-));
-$app->pipe($api = AppFactory::create());
+$app = AppFactory::create();
 
-$api->get('/get', function ($request, ResponseInterface $response, $next) {
-    /**
-     * @var Data $container
-     */
-    $container = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
-    $container->set('hello', $container->has('hello') ? $container->get('hello') + 1 : 0);
+$app
+    ->pipe(new SessionMiddleware(
+        new Sha256(),
+        $privateKey,
+        $publicKey,
+        \Dflydev\FigCookies\SetCookie::create('foo'),
+        new Parser(),
+        14400
+    ))
+    ->pipe($api = AppFactory::create())
+    ->get('/get', function ($request, ResponseInterface $response, $next) {
+        /* @var Data $container */
+        $container = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
+        $container->set('hello', $container->has('hello') ? $container->get('hello') + 1 : 0);
 
-    return $response->write($container->get('hello'));
-});
+        return $response->write($container->get('hello'));
+    });
 
 $app->run();
