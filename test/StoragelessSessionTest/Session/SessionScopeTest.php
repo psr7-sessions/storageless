bfe8664e38b4fa23691b31961b906f483d408d84
Jefersson Nathan <admin@phpse.net>
2015-10-27 06:14:37 -0300

    Added a valid test case using array with key

--- test/StoragelessSessionTest/Session/SessionScopeTest.php
+++ test/StoragelessSessionTest/Session/SessionScopeTest.php
@@ -75,6 +75,7 @@ final class SessionScopeTest extends PHPUnit_Framework_TestCase
     {
         return [
             ['foo', 'string'],
+            ['foo' => 'string'],
             ['foo#!@', new \stdClass()],
             ['foo123', []],
             ['foo bar', ''],
