c423ae73cfcfb8acf5247b38c5bc12f97a485930
Rudolph Gottesheim <r.gottesheim@loot.at>
2016-09-26 23:08:21 +0200

    Remove redundant @inheritdoc annotations

--- src/Storage/Session/StorableSession.php
+++ src/Storage/Session/StorableSession.php
@@ -43,76 +43,49 @@ class StorableSession implements StorableSessionInterface
         $this->storage = $storage;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getId() : SessionIdInterface
     {
         return $this->id;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function set(string $key, $value)
     {
         $this->wrappedSession->set($key, $value);
         $this->save();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function get(string $key, $default = null)
     {
         return $this->wrappedSession->get($key, $default);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function remove(string $key)
     {
         $this->wrappedSession->remove($key);
         $this->save();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function clear()
     {
         $this->wrappedSession->clear();
         $this->save();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function has(string $key) : bool
     {
         return $this->wrappedSession->has($key);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function hasChanged() : bool
     {
         return $this->wrappedSession->hasChanged();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function isEmpty() : bool
     {
         return $this->wrappedSession->isEmpty();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function jsonSerialize()
     {
         return $this->wrappedSession->jsonSerialize();
